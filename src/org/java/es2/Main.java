ade0264f5f2f50e71b4d6bdc14acd3eccdc36001
mcspe <mcspezzaferro@gmail.com>
2023-09-21 16:28:44 +0200

    [add] bonus esercizio 2

--- src/org/java/es2/Main.java
+++ src/org/java/es2/Main.java
@@ -1,12 +1,16 @@
 package org.java.es2;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// Esercizio 2
 
 		Scanner sc = new Scanner(System.in);
 		
@@ -15,14 +19,35 @@ public class Main {
 		
 		sc.close();
 		
-		Map<Character, Integer> charCounter = new HashMap<>();
+//		Map<Character, Integer> charCounter = new HashMap<>();
+//		for(int i = 0; i < text.length(); i++) {
+//			char k = text.charAt(i);
+//			if (charCounter.containsKey(k)) charCounter.put(k, charCounter.get(k) + 1);
+//			else charCounter.put(k, 1);
+//		}
+//		
+//		for(char c : charCounter.keySet()) System.out.println(c + ": " + charCounter.get(c));
+		
+		// Bonus
+		
+		List<Character> textCollection = new ArrayList<>();
 		for(int i = 0; i < text.length(); i++) {
 			char k = text.charAt(i);
-			if (charCounter.containsKey(k)) charCounter.put(k, charCounter.get(k) + 1);
-			else charCounter.put(k, 1);
+			textCollection.add(k);
 		}
 		
-		for(char c : charCounter.keySet()) System.out.println(c + ": " + charCounter.get(c));
+		textCollection.stream()
+    					.collect(Collectors.groupingBy(s -> s, TreeMap::new, Collectors.counting()))
+    					.forEach((k, v) -> System.out.println("'" + k + "':" + v));
+		
+		System.out.println(textCollection);
+		
+		// Bonus cheats
+		text.chars()
+    	.mapToObj(c -> (char) c)
+    	.map(Character::toLowerCase)
+    	.collect(Collectors.groupingBy(s -> s, TreeMap::new, Collectors.counting()))
+    	.forEach((k, v) -> System.out.println("'" + k + "':" + v));
 		
 	}
 
